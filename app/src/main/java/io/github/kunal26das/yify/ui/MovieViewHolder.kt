16d3a2ad512fe489cb098e6e498a2789c22e4d97
Kunal Das <kunal26das@gmail.com>
2021-10-26 02:42:07 +0530

    image load fix

--- app/src/main/java/io/github/kunal26das/yify/ui/MovieViewHolder.kt
+++ app/src/main/java/io/github/kunal26das/yify/ui/MovieViewHolder.kt
@@ -1,10 +1,7 @@
 package io.github.kunal26das.yify.ui
 
-import android.graphics.drawable.Drawable
 import android.view.ViewGroup
-import coil.load
 import coil.request.Disposable
-import coil.request.ImageRequest
 import io.github.kunal26das.yify.R
 import io.github.kunal26das.yify.databinding.ItemMovieBinding
 import io.github.kunal26das.yify.models.Movie
@@ -13,27 +10,13 @@ class MovieViewHolder(
     parent: ViewGroup,
 ) : ViewHolder<Movie, ItemMovieBinding>(parent, R.layout.item_movie) {
 
-    private var disposable: Disposable? = null
+    internal var disposable: Disposable? = null
 
-    fun bind(
-        item: Movie?,
-        image: Drawable? = null,
-        drawable: ((Drawable) -> Unit)? = null,
-    ): ItemMovieBinding? {
+    override fun bind(item: Movie?): ItemMovieBinding? {
         binding.movie = item
         disposable?.dispose()
         binding.executePendingBindings()
-        val lambda: ImageRequest.Builder.() -> Unit = {
-            if (drawable != null) target({}, {}, {
-                binding.image.setImageDrawable(it)
-                drawable.invoke(it)
-            })
-//            placeholderMemoryCacheKey(binding.image.metadata?.memoryCacheKey)
-        }
-        disposable = when (image) {
-            null -> binding.image.load(item?.largeCoverImage, imageLoader, lambda)
-            else -> binding.image.load(image, imageLoader, lambda)
-        }
+        binding.image.setImageDrawable(null)
         return super.bind(item)
     }
 
--- app/src/main/java/io/github/kunal26das/yify/ui/MoviesAdapter.kt
+++ app/src/main/java/io/github/kunal26das/yify/ui/MoviesAdapter.kt
@@ -3,22 +3,33 @@ package io.github.kunal26das.yify.ui
 import android.graphics.drawable.Drawable
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
+import coil.load
 import io.github.kunal26das.yify.models.Movie
 
 class MoviesAdapter : PagingDataAdapter<Movie, MovieViewHolder>(Movie) {
 
     private val images = mutableMapOf<Int, Drawable>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        return MovieViewHolder(parent)
-    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup, viewType: Int
+    ) = MovieViewHolder(parent)
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val movie = getItem(position)
-        if (movie != null) {
-            holder.bind(movie, images[movie.id]) {
-                images[movie.id] = it
+        val binding = holder.bind(movie)
+        if (movie != null && binding != null) {
+            holder.disposable = when (val image = images[movie.id]) {
+                null -> binding.image.load(movie.largeCoverImage) {
+                    target {
+                        binding.image.setImageDrawable(it)
+                        images[movie.id] = it
+                    }
+                }
+                else -> binding.image.load(image) {
+                    target { binding.image.setImageDrawable(it) }
+                }
             }
         }
     }
+
 }
