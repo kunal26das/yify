ca927d63f643c1ba7ff33109b967777e49cb1faf
Kunal Das <kunal26das@gmail.com>
2024-05-25 23:41:06 +0530

    version bump

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -5,7 +5,7 @@ object ProjectConfig {
     const val minSdk = 33
     const val targetSdk = 34
     const val compileSdk = 34
-    const val versionCode = 4
+    const val versionCode = 5
     const val versionName = versionCode.toString()
     val javaVersion = JavaVersion.VERSION_17
 }
