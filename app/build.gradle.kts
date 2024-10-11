36fbf3a00607326073ec9fa773cf06057b1500ca
Kunal Das <kunal26das@gmail.com>
2024-10-12 04:57:07 +0530

    ios fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,8 +77,8 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodel.ktx)
-            implementation(libs.androidx.lifecycle.runtime.compose)
+//            implementation(libs.androidx.lifecycle.viewmodel.ktx)
+//            implementation(libs.androidx.lifecycle.runtime.compose)
         }
     }
     sourceSets {
--- app/src/iosMain/kotlin/io/github/kunal26das/yify/MainViewController.kt
+++ app/src/iosMain/kotlin/io/github/kunal26das/yify/MainViewController.kt
@@ -2,4 +2,4 @@ package io.github.kunal26das.yify
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { }
