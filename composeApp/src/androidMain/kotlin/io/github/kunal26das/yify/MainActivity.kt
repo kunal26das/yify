8b323fa65c06a6d6d191544d184b39e3cc6e6523
Kunal Das <kunal26das@gmail.com>
2025-10-22 22:30:32 +0530

    fix android status bar

--- composeApp/src/androidMain/kotlin/io/github/kunal26das/yify/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/io/github/kunal26das/yify/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.kunal26das.yify
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialExpressiveTheme
@@ -16,6 +17,7 @@ import androidx.compose.material3.lightColorScheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MaterialExpressiveTheme(
                 colorScheme = when {
