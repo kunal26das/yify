a776ad92a4542fdd1d0feaca0daa454e85012f5e
Kunal Das <kunal26das@gmail.com>
2025-12-06 22:02:41 +0530

    add firebase dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     alias(libs.plugins.compose.compiler).apply(false)
     alias(libs.plugins.compose.hotreload).apply(false)
     alias(libs.plugins.compose.multiplatform).apply(false)
+    alias(libs.plugins.firebase.appdistribution).apply(false)
+    alias(libs.plugins.firebase.crashlytics).apply(false)
+    alias(libs.plugins.firebase.performance).apply(false)
     alias(libs.plugins.kotlin.multiplatform).apply(false)
     alias(libs.plugins.kotlin.serialization).apply(false)
 }
